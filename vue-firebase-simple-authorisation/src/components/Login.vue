2dc8873181b2fa55e38558e36066ecf6c14f6488
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2018-09-26 11:24:47 +0300

    app created

---
+++ vue-firebase-simple-authorisation/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="login">
+    <h3>Sign in</h3>
+    <input type="text" placeholder="Email"> <br>
+    <input type="password" placeholder="Password"> <br>
+    <button>Connetction</button>
+    <p>You dont`t have account? You can create one!</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: "login",
+    data: function() {
+        return{}
+    },
+    methods: {}
+}
+</script>
+
