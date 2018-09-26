9600a3a77cd8d8b8cdd26d2c977962b71fa7fdd6
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2018-09-26 11:56:57 +0300

    login component was created

--- vue-firebase-simple-authorisation/src/components/Login.vue
+++ vue-firebase-simple-authorisation/src/components/Login.vue
@@ -10,11 +10,38 @@
 
 <script>
 export default {
-    name: "login",
-    data: function() {
-        return{}
+  name: 'login',
+  data: function(){
+    return{}
     },
     methods: {}
 }
 </script>
+
+<style scoped>
+.login {
+    margin-top: 40px;
+}
+input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button {
+    margin-top: 20px;
+    width:10%;
+    cursor: pointer;
+}
+p {
+    margin-top: 40px;
+    font-size: 13px;
+}
+
+p a {
+    text-decoration: underline;
+    cursor: pointer
+}
+
+</style>
+
 
